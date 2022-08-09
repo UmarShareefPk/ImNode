ce689b857d2bbb07acfc21d0c5d1d8d3f844045f
UmarShareefPk <76015950+UmarShareefPk@users.noreply.github.com>
2022-08-10 03:44:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,60 +16,60 @@ const mongoose = require('mongoose');
 const usersRoutes = require('./routes/usersRoutes');
 const incidentsRoutes = require('./routes/incidentsRoutes');
 const notificationsRoutes = require('./routes/notificationsRoutes');
-const httpSocket = require('./socket'); 
-//var cors = require('cors');
-//const bodyParser = require('body-parser');
-//var jwt = require('jsonwebtoken');
+//const httpSocket = require('./socket'); 
+var cors = require('cors');
+const bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
 var config = require('./config');
 const port = process.env.PORT || 3333;
 
 //var error="";
 
-//app.use(cors());
+app.use(cors());
 
 app.listen(port);
 
-// connect to mongodb & listen for requests
-// const dbURI= "mongodb+srv://admin:pioneer007@cluster0.dg9t8.mongodb.net/IM?retryWrites=true&w=majority"
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(result => {
-//      app.listen(port);
-//   })
-//   .catch(err => {
-//     error = "There was an error witn MongoDB";
-//     app.listen(port);
-//     console.log(err)
-//   });
+//connect to mongodb & listen for requests
+const dbURI= "mongodb+srv://admin:pioneer007@cluster0.dg9t8.mongodb.net/IM?retryWrites=true&w=majority"
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+     app.listen(port);
+  })
+  .catch(err => {
+    error = "There was an error witn MongoDB";
+    app.listen(port);
+    console.log(err)
+  });
 
 ////////////// Middleware //////////////////////////
-//app.use(express.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
+app.use((req, res, next) => {
 
-//   if (req.path.toLowerCase() === "/users/login" || req.path.toLowerCase() === "/person"   || req.path.toLowerCase().includes("incidents/downloadfile")) {
-//     console.log("path: ", req.path);
-//     next();
-//     return;
-//   }
+  if (req.path.toLowerCase() === "/users/login" || req.path.toLowerCase() === "/person"   || req.path.toLowerCase().includes("incidents/downloadfile")) {
+    console.log("path: ", req.path);
+    next();
+    return;
+  }
 
-//   var token = req.headers["x-access-token"];
+  var token = req.headers["x-access-token"];
 
-//   if (!token)
-//     return res.status(401).send({ auth: false, message: "No token provided." });
+  if (!token)
+    return res.status(401).send({ auth: false, message: "No token provided." });
 
-//   jwt.verify(token, config.secret, function (err, decoded) {
-//     if (err)
-//       return res.status(401).send({ auth: false, message: "Error in authentication. Session expired or invalid. " });
+  jwt.verify(token, config.secret, function (err, decoded) {
+    if (err)
+      return res.status(401).send({ auth: false, message: "Error in authentication. Session expired or invalid. " });
 
-//     next();
-//   });
-// });
+    next();
+  });
+});
 
 ///////////////// Routes ///////////////////////////
-//app.use('/users', usersRoutes); 
-//app.use('/incidents', incidentsRoutes);
-//app.use('/notifications', notificationsRoutes);
+app.use('/users', usersRoutes); 
+app.use('/incidents', incidentsRoutes);
+app.use('/notifications', notificationsRoutes);
 
 //////////////  For Test //////////////////////////
 app.get('/person', (req, res) => {  
